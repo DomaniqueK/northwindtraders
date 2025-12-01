dbe9daece16ffc7a59ddaee66354523d269c13c3
DomaniqueK <domaniqueking24@gmail.com>
2025-12-01 11:36:40 -0600

    Exercise 2 workbook 8c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection connection;
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "Apples06");
+
+        Statement statement = connection.createStatement();
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products ";
+
+        ResultSet results = statement.executeQuery(query);
+        while (results.next()) {
+            String ProductID = results.getString("ProductID");
+            String ProductName= results.getString("ProductName");
+            String UnitPrice = results.getString("UnitPrice");
+            String UnitsInStock = results.getString("UnitsInStock");
+            System.out.println("ProductID: " + ProductID);
+            System.out.println("ProductName: " + ProductName);
+            System.out.println("Unit Price: " + UnitPrice);
+            System.out.println("Units In Stock: " + UnitsInStock);
+
+        } connection.close();
+    }
 }
