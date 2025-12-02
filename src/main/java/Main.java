730057c55dfea29ff37ba84b23b3af9a3f2f6fd0
DomaniqueK <domaniqueking24@gmail.com>
2025-12-02 09:52:37 -0600

    Added data source

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -5,19 +7,27 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     private static Object query;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException {
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Apples06");
 
         String homeMenu = "" +
                 "\n 1) Display all products \n" +
                 " 2) Display all customers \n" +
+                " 3) Display all categories \n " +
                 " 0) Exit \n" +
                 " Select and option\n ";
         System.out.println(homeMenu);
         int choice = scanner.nextInt();
 
 
-        try (
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "Apples06")) {
+        try (Connection connection = dataSource.getConnection()){// Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "Apples06")) {
+
+
             if (choice == 1) {
 
                 Statement statement = connection.createStatement();
@@ -35,10 +45,18 @@ public class Main {
                     System.out.println("Units In Stock: " + UnitsInStock);
 
                 }
-            } else if (choice == 2) {
-                Statement statement = connection.createStatement();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            if (choice == 2) {
+
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT First_name, Last_name FROM Customer " + "WHERE Last_name LIKE ? ORDER BY First_name") ;
                 String query = "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ORDER BY Country";
-                ResultSet results = statement.executeQuery(query);
+                ResultSet results = preparedStatement.executeQuery(query);
 
                 while (results.next()) {
                     String CompanyName = results.getString("CompanyName");
@@ -51,11 +69,19 @@ public class Main {
                     System.out.println("City: " + City);
                     System.out.println("Country: " + Country);
                     System.out.println("Phone number: " + Phone);
+                }  if (choice == 3 ) {
+                    String CategoryID = results.getString("CategoryID");
+                    String CategoryName = results.getString("CategoryName");
+                    System.out.println("Contact name: " + CategoryID);
+                    System.out.println("Contact name: " + CategoryName);
                 }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
+
+        }
 }
+
+
+
